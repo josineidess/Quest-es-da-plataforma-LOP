858c8cc2b074bca8a91c02e076650f5983e6619e
JosineideSS <josineide1402@gmail.com>
2020-08-12 04:52:39 -0300

    Cod: 345e801453 Nível: Muito fácil

---
+++ Norma do vetor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+ float a;
+ float b;
+int main () {
+    cin >> a;
+    cin >> b;
+    float media= sqrt(pow (a,2)+pow (b,2));
+    cout<<media;
+    return 0;
+}
