f92077573c4c97f5a0e949a3f302ae509174a818
JosineideSS <josineide1402@gmail.com>
2020-08-12 05:00:02 -0300

    Cod: ad85213427 Nível: Muito fácil

---
+++ Perfuradora hidraulica.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+float p1;
+float p2;
+float p3;
+int main(){
+    cin >> p1;
+    cin >> p2;
+    cin >> p3;
+    cout <<(p1+p2+p3)/3;
+    return 0;
+}
