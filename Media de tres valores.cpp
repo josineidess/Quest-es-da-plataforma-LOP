20894021f71aa0e9b3d2e82672d65cb9ab2c836c
JosineideSS <josineide1402@gmail.com>
2020-08-12 04:45:30 -0300

    Cod: dfd5b01b42 Nível: Muito fácil

---
+++ Media de tres valores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+float n1,n2,n3;
+float media;
+
+int main() {
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+    media = (n1+n2+n3)/3;
+    cout << media << "\n";
+    return 0;
+}
