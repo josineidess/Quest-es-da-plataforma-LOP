2eb901f34c02930a3b5ebeeae830822ab2ded3bc
JosineideSS <josineide1402@gmail.com>
2020-08-12 05:03:36 -0300

    Cod: f61c3164a8 Nível: Fácil

---
+++ L1Q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+float a;
+float b;
+float c;
+float d;
+float e;
+float f;
+int main() {
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+    cout << (b*(c+a))/(d*(e+f));
+    return 0;
+}
